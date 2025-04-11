c46ce42a6987b4ceeae7fbfa53efa8be610f0577
Khyati <khyatisikarwar22@gmail.com>
2025-04-11 20:53:59 +0530

    Fix relative import for model

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict
-from model import predict_cost
+from .model import predict_cost
+
 
 app = FastAPI()
 
